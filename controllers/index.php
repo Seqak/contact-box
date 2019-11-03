8396b58bafffcb07f9c79443fa95176e01753b9c
Kacper Koziol <kacperu770@gmail.com>
2019-11-03 01:26:52 +0100

    Add Twig to view. Start fields validate

--- controllers/index.php
+++ controllers/index.php
@@ -8,47 +8,51 @@ use PHPMailer\PHPMailer\SMTP;
 
 require('../vendor/autoload.php');
 
-if (isset($_POST['send-btn'])) {
+// if (isset($_POST['send-btn'])) {
     
-    $mail = new PHPMailer();
+//     $mail = new PHPMailer();
 
-    $mail->isSMTP();
-    $mail->Host = "smtp.gmail.com";
-    $mail->SMTPAuth = true;
-    $mail->Username = "jonbcrypt@gmail.com";
-    $mail->Password = '#Hash123';
-    $mail->SMTPSecure = "ssl";
-    $mail->Port = 465;
+//     $mail->isSMTP();
+//     $mail->Host = "smtp.gmail.com";
+//     $mail->SMTPAuth = true;
+//     $mail->Username = "jonbcrypt@gmail.com";
+//     $mail->Password = '#Hash123';
+//     $mail->SMTPSecure = "ssl";
+//     $mail->Port = 465;
 
 
-    $mail->isHTML(true);
-    $mail->setFrom("jonbcrypt@gmail.com", "Kacper");
-    $mail->addAddress("kacperu770@gmail.com", "Test");
-    $mail->Subject = "Test e-mail2";
-    $mail->Body = "Body of email -lorem ipsum dolor sit amet";
-    // ktesty@wp.pl
+//     $mail->isHTML(true);
+//     $mail->setFrom("jonbcrypt@gmail.com", "Kacper");
+//     $mail->addAddress("kacperu770@gmail.com", "Test");
+//     $mail->Subject = "Siema";
+//     $mail->Body = "Body of email -lorem ipsum dolor sit amet";
+//     // ktesty@wp.pl
 
-    if ($mail->send()) {
-        echo "Mail is sent";
+//     if ($mail->send()) {
+//         // echo "Mail is sent";
+//     }
+//     else{
+//         // echo 'Mailer error: ' . $mail->ErrorInfo;
+//         // echo "Something is wrong";
+//     }
+
+// }
+// else{      
+// }
+// $addressError = false;
+
+if (isset($_POST['receiver-field'])) {
+    
+    $odbiorca = $_POST['receiver-field'];
+    if ($odbiorca > 3) {
+        $addressError = true;
     }
     else{
-        echo 'Mailer error: ' . $mail->ErrorInfo;
-        echo "Something is wrong";
-    }
-
+        $addressError = false;
+    }   
 }
 else{
-
-        
-}
-
-if (isset($_POST['odbiorca-in'])) {
-    // echo "tekstyyy: " . $_POST['odbiorca-in'];
-    $_SESSION['test'] = $_POST['odbiorca-in'];
-    
-}
-else{
-    $_SESSION['test'] = "Nie działa";
+   
 }
 
 
@@ -63,7 +67,7 @@ echo $twig->render('index.html', array(
     'age' => 3,
     'title' => $title,
     'title2' => $title2,
-    'test' => $_SESSION['test'],
+    'error1' => $addressError,
     
 ));
 
